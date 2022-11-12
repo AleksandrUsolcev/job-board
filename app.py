0d5863224661db285b16de2e59b5011ac396e426
Aleksandr Usolcev <alexusolev@gmail.com>
2022-11-12 12:48:48 +0300

    login page, add staticfiles

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'change-me'
 
 
 @app.route('/')
@@ -13,9 +15,21 @@ def index():
     return render_template('index.html', **context)
 
 
-@app.route('/patients')
-def patients_list():
-    return render_template('patients_list.html')
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        if request.form.get('password') != '12345':
+            flash('Неверный пароль', category='error')
+    context = {}
+    return render_template('login.html', **context)
+
+
+@app.route('/patients/<int:id>')
+def patients_list(id):
+    context = {
+        'patient': id
+    }
+    return render_template('patient_page.html', **context)
 
 
 if __name__ == '__main__':
