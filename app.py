0dc929a72915fc1c71c4127f146b2a6dc6a02aaf
Aleksandr Usolcev <alexusolev@gmail.com>
2022-11-12 10:13:21 +0300

    templates

--- app.py
+++ app.py
@@ -5,7 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    menu = ['test1', 'test2', 'test3']
+    context = {
+        'title': 'hello world',
+        'menu': menu,
+    }
+    return render_template('index.html', **context)
+
+
+@app.route('/patients')
+def patients_list():
+    return render_template('patients_list.html')
 
 
 if __name__ == '__main__':
