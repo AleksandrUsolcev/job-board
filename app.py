a881ccab96287ef7dbfdbb6d0a5376401e6b711b
Aleksandr Usolcev <alexusolev@gmail.com>
2022-11-13 07:53:59 +0300

    errorhandler

--- app.py
+++ app.py
@@ -1,15 +1,25 @@
+from dotenv import load_dotenv
 from flask import (Flask, abort, flash, redirect, render_template, request,
                    session, url_for)
+
+load_dotenv()
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'change-me'
 
 
-@app.errorhandler(404)
-def not_found(error):
-    context = {'error': error, 'error_code': 404}
-    return render_template('error_page.html', **context), 404
+@app.errorhandler(Exception)
+def error_page(error: Exception):
+    error_list = [404, 401]
+    if error.code not in error_list:
+        return error
+    context = {
+        'error': error.description,
+        'error_code': error.code,
+        'error_name': error.name
+    }
+    return render_template('error_page.html', **context), error.code
 
 
 @app.route('/')
@@ -26,31 +36,39 @@ def index():
 def login():
     test_password = '12345'
     test_email = 'admin@admin.admin'
-    test_token = 'adksmdamasdmsakldmalsm'
     if request.method == 'POST':
         if request.form.get('password') != '12345':
             flash(request.form.get('email'), category='email')
             flash('Неверный пароль', category='error')
     context = {}
-    if 'token' in session:
+    if 'email' in session:
         return redirect(url_for('index'))
     elif (
         request.method == 'POST'
         and request.form.get('email') == test_email
         and request.form.get('password') == test_password
     ):
-        session['token'] = test_token
+        session['email'] = test_email
         return redirect(url_for('index'))
     return render_template('login.html', **context)
 
 
-@app.route('/patients/<int:id>')
-def patients_list(id):
-    check_token = session.get('token')
-    if not check_token:
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@app.route('/job/<category>/<int:id>')
+def vacancy_detail(category, id):
+    check_email = session.get('email')
+    if not check_email:
         abort(401)
-    context = {'patient': id}
-    return render_template('patient_page.html', **context)
+    context = {
+        'vacancy': id,
+        'category': category
+    }
+    return render_template('vacancy_detail.html', **context)
 
 
 if __name__ == '__main__':
