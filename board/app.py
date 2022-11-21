4a50f0dc21e5c54104867674eb43ab74a9524be1
Aleksandr Usolcev <alexusolev@gmail.com>
2022-11-21 19:48:13 +0300

    update templates

--- board/app.py
+++ board/app.py
@@ -5,6 +5,7 @@ from app.workspace import workspace
 app.register_blueprint(admin, url_prefix='/admin')
 app.register_blueprint(workspace, url_prefix='/workspace')
 
+
 @app.shell_context_processor
 def make_shell_context():
     context = {'db': db}
--- board/app/workspace/routes.py
+++ board/app/workspace/routes.py
@@ -2,9 +2,11 @@ from app.models import Candidate, Category, Vacancy
 from app.workspace import workspace
 from flask import render_template
 from sqlalchemy import func
+from flask_login import login_required
 
 
 @workspace.route('/')
+@login_required
 def index():
     candidates = Candidate.query.all()
     categories = Category.query.outerjoin(Vacancy).group_by(
